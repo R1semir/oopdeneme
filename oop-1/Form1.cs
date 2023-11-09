7bb7a21c06f911d9499dc4a5740003e4819f9554
Emir <emirkan.ulker9@gmail.com>
2023-11-09 12:11:36 +0300

    v1

---
+++ oop-1/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace oop_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ev evim = new ev();
+
+            evim.turu = "Daire";
+            evim.renk = "Beyaz";
+            evim.odasayisi = 5;
+            evim.kat = 6;
+
+            string turu = evim.evigöster();
+
+            label5.Text = evim.turu;
+            label6.Text = evim.renk;
+            label7.Text = evim.odasayisi.ToString();
+            label8.Text = evim.kat.ToString();
+        }
+    }
+}
---
+++ oop-1/ev.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop_1
+{
+    internal class ev
+    {
+
+        public string turu { get; set; }
+
+        public string renk { get; set; }
+
+        public int odasayisi { get; set; }
+
+        public int kat { get; set; }
+
+        public string evigöster()
+        {
+            return turu;
+        }
+
+    }
+}
